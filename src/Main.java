84d1f960b25264c763ffbc87536d5198feaf64e7
Ruslan.Shine <ruslan.shainuroff@yandex.ru>
2022-05-25 17:10:58 +0500

    test

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Workbench workbench = new Workbench("Gigant",100);
+        workbench.start();
+        workbench.delaemDetal(50);
+        workbench.delaemDetal(100);
+        workbench.refuel(50);
+        workbench.delaemDetal(100);
+
+
+    }
+}
---
+++ src/Workbench.java
@@ -0,0 +1,35 @@
+public class Workbench {
+    private String name;
+    private int oil;
+    private boolean isOff;
+
+    public Workbench(String name, int oil) {
+        this.name = name;
+        this.oil = oil;
+    }
+    public void start(){
+        this.isOff=false;
+    }
+    public void delaemDetal (int liters){
+        if (oil>=liters){
+            oil-= liters;
+            System.out.println(name +": Работа сделана!" );
+        } else if (oil<liters){
+            System.out.println(name +": Масло закончилось !" );
+            isOff =true;
+        }
+    }
+
+    public void refuel(int refOil){
+        oil+=refOil;
+        System.out.println(name +": Готов к работе!");
+        isOff= false;
+    }
+
+
+
+
+
+}
+
+
